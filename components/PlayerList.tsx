57d8098733150f0600974eddced9c7aec8d03b9e
lmfjeff <lmfjeff@gmail.com>
2024-09-11 18:17:24 +1000

    add search character by name, change layout, pagination, debounce input

--- components/PlayerList.tsx
+++ components/PlayerList.tsx
@@ -1,46 +1,98 @@
 "use client";
 
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import Loading from "./Loading";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function PlayerList() {
   const [characters, setCharacters] = useState([]);
+  const [paginationData, setPaginationData] = useState<any>({});
+  const [fetching, setFetching] = useState(false);
+  const [page, setPage] = useState(1);
+  const [searchInput, setSearchInput] = useState("");
+
+  const totalPage = useMemo(
+    () => Math.ceil(paginationData.total / paginationData.limit),
+    [paginationData]
+  );
+  const currentPage = useMemo(
+    () => paginationData.offset / paginationData.limit + 1,
+    [paginationData]
+  );
+  const isFirstPage = useMemo(() => currentPage === 1, [currentPage])
+  const isLastPage = useMemo(() => currentPage === totalPage, [currentPage, totalPage])
+
+  const debounced = useDebouncedCallback((value) => {
+    setSearchInput(value);
+    setPage(1);
+  }, 1000);
+
+  async function fetchCharacters(searchInput: string, page: number) {
+    setFetching(true);
+    const resp = await axios.get(
+      "https://gateway.marvel.com:443/v1/public/characters",
+      {
+        params: {
+          apikey: process.env.NEXT_PUBLIC_MARVEL_PUBLIC_KEY,
+          ...(searchInput ? { nameStartsWith: searchInput } : {}),
+          ...(paginationData.limit
+            ? { offset: (page - 1) * paginationData.limit }
+            : {}),
+        },
+      }
+    );
+    setFetching(false);
+
+    const charactersData = resp.data.data;
+    const { total, offset, limit, count, results } = charactersData;
+    setPaginationData({ total, offset, limit, count });
+    setCharacters(results);
+  }
+
   useEffect(() => {
-    async function fetchCharacters() {
-      const resp = await axios.get(
-        "https://gateway.marvel.com:443/v1/public/characters",
-        {
-          params: {
-            apikey: process.env.NEXT_PUBLIC_MARVEL_PUBLIC_KEY,
-          },
-        }
-      );
-      const charactersData = resp.data.data;
-      const { totals, results } = charactersData;
-      setCharacters(results);
-    }
-    if (characters.length === 0) fetchCharacters();
-  }, []);
+    fetchCharacters(searchInput, page);
+  }, [searchInput, page]);
+
   return (
-    <div className="w-[400px] bg-gray-500">
-      <div>player list</div>
-      <div className="flex flex-wrap">
-        {characters.map((c: any) => (
-          <>
-            {c.thumbnail.path.includes("image_not_available") ? (
-              <div key={c.id} className="border rounded-full size-[100px] flex justify-center items-center">
-                <div className="text-sm text-center">{c.name}</div>
-              </div>
-            ) : (
-              <img
-                key={c.id}
-                src={`${c.thumbnail.path}.${c.thumbnail.extension}`}
-                className="rounded-full size-[100px]"
-              />
-            )}
-          </>
-        ))}
+    <div className="w-full max-w-[400px] bg-gray-500">
+      <div className="flex justify-between">
+        <input
+          placeholder="search..."
+          onChange={(e) => debounced(e.target.value)}
+        />
+        {characters.length > 0 && !fetching && (
+          <div className="flex gap-2">
+            {!isFirstPage && <button onClick={() => setPage((v) => v - 1)}>◀</button>}
+            <div>
+              Page {currentPage} / {totalPage}
+            </div>
+            {!isLastPage && <button onClick={() => setPage((v) => v + 1)}>▶</button>}
+          </div>
+        )}
       </div>
+      {fetching ? (
+        <div className="flex justify-center">
+          <Loading />
+        </div>
+      ) : (
+        <div className="flex flex-wrap">
+          {characters.map((c: any) => (
+            <div key={c.id}>
+              {c.thumbnail.path.includes("image_not_available") ? (
+                <div className="border rounded-full size-[100px] flex justify-center items-center">
+                  <div className="text-sm text-center">{c.name}</div>
+                </div>
+              ) : (
+                <img
+                  src={`${c.thumbnail.path}.${c.thumbnail.extension}`}
+                  className="border rounded-full size-[100px]"
+                />
+              )}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
