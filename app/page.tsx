60cf57b71b6c1d060cb7ac7bcb165b85dbcd5e4d
lmfjeff <lmfjeff@gmail.com>
2024-09-13 03:18:27 +1000

    bugfix: suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Container from "@/components/Container";
 import PlayerList from "@/components/PlayerList";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
@@ -13,7 +14,9 @@ export default function Home() {
         </div>
       </div>
       <div className="w-full flex flex-col md:flex-row justify-center items-center md:items-start">
-        <Container />
+        <Suspense>
+          <Container />
+        </Suspense>
       </div>
     </div>
   );
